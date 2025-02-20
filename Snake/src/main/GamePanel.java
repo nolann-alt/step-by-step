618401b929137f1be3d576c4ff2cc5bbbbb85279
nolann-alt <lescopnolann@gmail.com>
2025-02-20 19:58:58 +0100

    Update

--- Snake/src/main/GamePanel.java
+++ Snake/src/main/GamePanel.java
@@ -1,21 +1,59 @@
 package main;
-import javax.swing.JFrame; // Import de Swing
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * This class allows to create a window for the game Snake
  */
-public class GamePanel extends JFrame{
+public class GamePanel extends JPanel{
 
+    private ArrayList<Point> snakeBody; // Permet de créer un tableau de point
+    private int dx, dy; // Permet de définir la direction du serpent avec dx la direction horizontale et dy la direction verticale
+
+    /**
+     * Constructor of the class GamePanel
+     */
     public GamePanel() {
-        super("Snake"); // 1 Définir un titre pour la fenêtre
-        setSize(500, 400); // 2 Définir une taille (exemple : 500x400)
-        // 3 Indiquer quoi faire quand on clique sur la croix (fermer l'application)
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true); // 4 Afficher la fenêtre
+        setBackground(new Color(62,  111,  62)); // Définis la couleur de fond de la fenêtre
+
+        snakeBody = new ArrayList<>(); // Permet de créer un nouveau tableau de point
+        snakeBody.add(new Point(100, 100)); // Permet d'ajouter un point au tableau de point
+
+        dx = 10; // Permet de définir la direction horizontale du serpent
+        dy = 0; // Permet de définir la direction verticale du serpent
+
+        Timer timer = new Timer(100, actionEvent -> moveSnake());
+        timer.start();
     }
 
-    // Méthode principale (main) qui va lancer la fenêtre
-    public static void main(String[] args) {
-        new GamePanel();
+    @Override
+    /**
+     * This method allows to draw the snake on the window
+     */
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Permet d'effacer l'écran avant de redessiner
+
+        // Parcours l'arrayList snakeBody ainsi que chacun de ses segment afin de les dessinés
+        for (Point segment : snakeBody) {
+            g.fillRect(segment.x, segment.y, 20, 20); // Dessine le cube
+            g.setColor(Color.white); // Définis la couleur du cube
+        }
+    }
+
+    /**
+     * This method allows to move the snake on the window with the direction dx and dy
+     */
+    public void moveSnake() {
+        Point head = snakeBody.get(0); // On récupère la tête du snake
+        head.x += dx; // On déplace la tête horizontalement en ajoutant dx
+        head.y += dy; // On déplace la tête verticalement en ajoutant dy
+
+        for (int i = snakeBody.size(); i > 0; i--) {
+            Point current = snakeBody.get(i); // on récupère le cube actuel
+            Point previous = snakeBody.get(i-1); // on récupère le cube précédent
+            current.setLocation(previous.x, previous.y); // Pemet de déplacer le cube actuel à la place du précédent afin que le snake avance
+        }
     }
 }
--- Snake/src/main/Main.java
+++ Snake/src/main/Main.java
@@ -1,10 +1,25 @@
 package main;
 
+import modele.Snake;
+
+import javax.swing.*;
+
 /**
- *
+ * This class allow to start the snake game
  */
 public class Main {
+
+    // Méthode principale (main) qui va lancer la fenêtre
     public static void main(String[] args) {
-        System.out.println("Bienvenue");
+        JFrame window = new JFrame("Snake"); // Pemet de crée une nouvelle fenêtre
+        window.setSize(1200, 1200); // Taille de la fenêtre
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Permet de fermer la fenêtre et terminer l'execution en même temps
+        window.setResizable(false);
+        window.setLocationRelativeTo(null); // Centre la fenêtre
+
+        GamePanel panel = new GamePanel(); // Permet de faire un nouveau panneau graphique sur lequel on peut dessiner avec la class GamePanel
+        window.add(panel); // Ajoute le panneau graphique à la fenêtre
+
+        window.setVisible(true);
     }
 }
