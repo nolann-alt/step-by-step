34352cd737215c4be16ead545be989b5395f53c4
nolann-alt <lescopnolann@gmail.com>
2025-03-02 00:52:30 +0100

    Update

--- Snake/src/main/GamePanel.java
+++ Snake/src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import modele.Snake;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -9,8 +11,7 @@ import java.util.ArrayList;
  */
 public class GamePanel extends JPanel{
 
-    private ArrayList<Point> snakeBody; // Permet de créer un tableau de point
-    private int dx, dy; // Permet de définir la direction du serpent avec dx la direction horizontale et dy la direction verticale
+    private Snake snake;
 
     /**
      * Constructor of the class GamePanel
@@ -18,14 +19,16 @@ public class GamePanel extends JPanel{
     public GamePanel() {
         setBackground(new Color(62,  111,  62)); // Définis la couleur de fond de la fenêtre
 
-        snakeBody = new ArrayList<>(); // Permet de créer un nouveau tableau de point
-        snakeBody.add(new Point(100, 100)); // Permet d'ajouter un point au tableau de point
+        snake = new Snake(); // Permet de créer un nouveau tableau de point
 
-        dx = 10; // Permet de définir la direction horizontale du serpent
-        dy = 0; // Permet de définir la direction verticale du serpent
-
-        Timer timer = new Timer(100, actionEvent -> moveSnake());
+        // Configure un Timer pour déplacer le serpent toutes les 100 ms
+        Timer timer = new Timer(100, actionEvent -> {
+            snake.move(); // Déplace le serpent
+            repaint(); // Redessine le panneau
+        });
         timer.start();
+
+        // Ajouter le keyListener
     }
 
     /**
@@ -37,19 +40,13 @@ public class GamePanel extends JPanel{
         super.paintComponent(g); // Permet d'effacer l'écran avant de redessiner
 
         // Parcours l'arrayList snakeBody ainsi que chacun de ses segment afin de les dessinés
-        for (Point segment : snakeBody) {
-            g.fillRect(segment.x, segment.y, 20, 20); // Dessine le cube
+        for (Point segment : snake.getSnakeBody) {
             g.setColor(Color.white); // Définis la couleur du cube
+            g.fillRect(segment.x, segment.y, 20, 20); // Dessine le cube
+
         }
     }
 
-    /**
-     * This method allows to move the snake on the window with the direction dx and dy
-     */
-    public void moveSnake() {
-        Point head = snakeBody.get(0); // récupère la tête actuel
-        Point newHead = new Point(head.x + dx, head.y + dy); // fait une nouvelle tête en ajoutant dx et dy
-        snakeBody.add(0, newHead); // ajoute la nouvelle tête
-        snakeBody.remove(snakeBody.size()-1); // enlève le dernier cube
-    }
+
+
 }
--- Snake/src/modele/Snake.java
+++ Snake/src/modele/Snake.java
@@ -1,10 +1,48 @@
 package modele;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
- *
+ * This class allows to manage the movement of the snake on the window
  */
 public class Snake {
-    public static void main(String[] args) {
+
+    /** The body of the snake */
+    private ArrayList<Point> snakeBody;
+    /** The direction of the snake */
+    private int dx, dy;
+    /** The size of the segment */
+    private final int segmentSize = 20;
+
+    /**
+     * Constructor of the class Snake
+     */
+    public Snake(){
+
+        snakeBody = new ArrayList<>(); // Permet de créer un nouveau tableau de point
+        snakeBody.add(new Point(100, 100));
+
+        dx = 10; // Permet de définir la direction horizontale du serpent
+        dy = 0; // Permet de définir la direction verticale du serpent
+    }
+
+    /**
+     * This method allows to move the snake on the window with the direction dx and dy and the keyListener
+     */
+    public void move() {
+        Point head = snakeBody.get(0); // Récupère la tête actuelle
+        Point newHead = new Point(head.x + dx, head.y + dy); // Calcule la nouvelle tête
+        snakeBody.add(0, newHead); // Ajoute la nouvelle tête
+        snakeBody.remove(snakeBody.size() - 1); // Supprime le dernier segment
+    }
+
+    public void changeDirection() {
 
     }
+
+    public ArrayList<Point> getSnakeBody () {
+        return this.snakeBody;
+    }
+
 }
